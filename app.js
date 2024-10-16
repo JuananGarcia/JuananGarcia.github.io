34732dea1983bea13596600a28f35baa7719f7f1
JuananGarcia <55695749+JuananGarcia@users.noreply.github.com>
2024-10-16 15:40:56 +0200

    restore

--- app.js
+++
@@ -1,4 +0,0 @@
-function showDetails(burgerName) {
-    // Redirigir a la página de detalles con el nombre de la hamburguesería seleccionada
-    window.location.href = `details.html?burger=${burgerName}`;
-}
